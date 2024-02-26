038c99c126995bc8fe3bdc89cd6b1102fa05d1ea
Mark Phillips <phillipsmb@cardiff.ac.uk>
2024-02-26 21:39:36 +0000

    GitHub commit test

---
+++ Flashcards2/DSA/NavController/NavController/NavControllerApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavControllerApp.swift
+//  NavController
+//
+//  Created by Mark Phillips on 24/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct NavControllerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
